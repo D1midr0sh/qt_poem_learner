47bc7b4ccb54df22cd2792fa7d088332c38af463
Dmitriy Kirichenko <dimidrosh@gmail.com>
2023-11-19 11:59:10 +0300

    Проверка правильности Теперь программа проверяет правильность введённой части стиха, а также показывает процент правильности.

--- learn.py
+++ learn.py
@@ -1,7 +1,7 @@
-from PyQt5 import uic
+from PyQt5 import QtCore, uic
 from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QWidget
-from ut import algorithm, divide
+from ut import algorithm, divide, similar
 
 
 class Learn(QWidget):
@@ -10,6 +10,7 @@ class Learn(QWidget):
         uic.loadUi("designs/learn.ui", self)
         self.count = -1
         self.proceedButton.clicked.connect(self.start_learning)
+        self.checkButton.clicked.connect(self.check_correct)
 
     def setVariables(self, poem: list, verse_size: int):
         self.verse_size = verse_size
@@ -17,9 +18,62 @@ class Learn(QWidget):
         self.needed = algorithm(self.poem)
 
     def start_learning(self):
+        self.checking.setText("")
+        self.output.setFont(QFont("MS Shell Dlg", 24))
         self.count += 1
         if self.count >= len(self.needed):
             self.output.setText("Выучил. Молодец!")
-        self.output.setText('\n'.join(self.needed[self.count]))
+            return
+        self.output.setText("\n".join(self.needed[self.count]))
         if len(self.needed[self.count]) > 4:
-            self.output.setFont(QFont("MS Shell Dlg", 16))
+            if len(self.needed[self.count]) > 12:
+                self.output.setFont(QFont("MS Shell Dlg", 12))
+            elif len(self.needed[self.count]) > 20:
+                self.output.setFont(QFont("MS Shell Dlg", 8))
+            else:
+                self.output.setFont(QFont("MS Shell Dlg", 16))
+            self.label.setText("Повторение")
+            self.proceedButton.setVisible(False)
+            tim = QtCore.QTimer()
+            tim.setInterval(15000)
+            tim.start()
+            while tim.isActive():
+                self.checking.setText(f"Осталось {tim.remainingTime() // 1000} секунд.")
+                if tim.remainingTime() < 10:
+                    tim.stop()
+                QtCore.QCoreApplication.processEvents()
+            self.proceedButton.setVisible(True)
+            self.stackedWidget.setCurrentWidget(self.stackedWidget.widget(1))
+
+    def check_correct(self):
+        self.stackedWidget.setCurrentWidget(self.stackedWidget.widget(2))
+        to_check_with = (
+            "\n".join(self.needed[self.count]).replace(".", "").replace(",", "")
+        )
+        to_check_with = (
+            to_check_with.replace("!", "")
+            .replace("?", "")
+            .replace(":", "")
+            .replace(";", "")
+        )
+        to_check_with = (
+            to_check_with.replace("-", "").replace("(", "").replace(")", "").lower()
+        )
+        ch = (
+            self.check.toPlainText()
+            .replace(".", "")
+            .replace(",", "")
+            .replace("!", "")
+            .replace("?", "")
+        )
+        ch = (
+            ch.replace(":", "")
+            .replace(";", "")
+            .replace("-", "")
+            .replace("(", "")
+            .replace(")", "")
+            .lower()
+        )
+        self.result.setText(
+            f"Отношение правильности: {round(similar(to_check_with, ch) * 100, 2)}%"
+        )
--- ut.py
+++ ut.py
@@ -1,19 +1,17 @@
+from difflib import SequenceMatcher
 from math import log2
 
 
-def read_poem(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        poem = file.read().splitlines()
-
-    return poem
+def similar(a: str, b: str) -> float:
+    return SequenceMatcher(None, a, b).ratio()
 
 
-def divide(lst, n): 
-    for i in range(0, len(lst), n):  
-        yield lst[i:i + n] 
+def divide(lst: list, n: int):
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
 
 
-def algorithm(verses: list):
+def algorithm(verses: list) -> list:
     ans = []
     for i in range(1, len(verses) + 1):
         verse = verses[i - 1]
@@ -25,14 +23,14 @@ def algorithm(verses: list):
         if ans[-1] != verse:
             ans.append(verse)
         if i % 2 == 0:
-            print('CHECK')
+            print("CHECK")
             to_check = []
             count = int(log2(i))
-            if 2 ** count == i:
+            if 2**count == i:
                 for v in verses[:i]:
                     to_check.extend(v)
             else:
-                for v in verses[i - 2 ** count + 1:i]:
+                for v in verses[i - 2**count + 1 : i]:
                     to_check.extend(v)
             ans.append(to_check)
     return ans
